8dee54b44b748ac67d7d814c91e6af98547b9e77
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-12-28 13:48:43 -0300

    chore: basic actix setup

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate log;
+
+use dotenv::dotenv;
+use std::env;
+
+use actix_web::{middleware, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  std::env::set_var("RUST_LOG", "proglog=trace");
+
+  dotenv().ok();
+
+  env_logger::init();
+
+  let host = env::var("HOST").unwrap();
+  let port = env::var("PORT").unwrap().parse::<u16>().unwrap();
+
+  info!("starting server at {}:{}", &host, port);
+
+  HttpServer::new(move || App::new().wrap(middleware::Logger::default()))
+    .bind((host, port))?
+    .run()
+    .await
+}
